3e399197509712ce3d85d33d2f6ea6ae42e8bb37
Zheka Chakhov <zhekachakhov@mail.ru>
2023-06-06 18:14:16 +0300

    add numbers style

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -9,22 +9,22 @@ export const Board = (
   return (
     <div className="Board">
       <div className="board-row">
-        <Cell />
-        <Cell />
-        <Cell />
-        <Cell />
+        <Cell value={2} />
+        <Cell value={4} />
+        <Cell value={8} />
+        <Cell value={16} />
       </div>
       <div className="board-row">
-        <Cell />
-        <Cell />
-        <Cell />
-        <Cell />
+        <Cell value={32} />
+        <Cell value={64} />
+        <Cell value={128} />
+        <Cell value={256} />
       </div>
       <div className="board-row">
-        <Cell />
-        <Cell />
-        <Cell />
-        <Cell />
+        <Cell value={512} />
+        <Cell value={1024} />
+        <Cell value={2048} />
+        <Cell value={2} />
       </div>
       <div className="board-row">
         <Cell />
--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 
 export const Cell = ({ value }) => {
-  return (
-    <div className="Cell">
-      <p>{value}</p>
-    </div>
-  );
+  const cellClass = (value) => {
+    if (+value == 2) return "n-2";
+    else if (+value == 4) return "n-4";
+    else if (+value == 8) return "n-8";
+    else if (+value == 16) return "n-16";
+    else if (+value == 32) return "n-32";
+    else if (+value == 64) return "n-64";
+    else if (+value == 128) return "n-128";
+    else if (+value == 256) return "n-256";
+    else if (+value == 512) return "n-512";
+    else if (+value == 1024) return "n-1024";
+    else if (+value == 2048) return "n-2048";
+  };
+
+  return <div className={`Cell ${cellClass(value)}`}>{value}</div>;
 };
